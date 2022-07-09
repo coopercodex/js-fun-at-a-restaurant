4e824dc96051914e9bb817c53636477744ab8e27
Derek Cooper <derekcooper20@gmail.com>
2022-07-08 21:43:44 -0600

    Initial commit

--- src/order.js
+++ src/order.js
@@ -1,15 +1,33 @@
-function takeOrder() {
-  
-}
+function takeOrder(item, deliveryOrder) {
+  if (deliveryOrder.length < 3) {
+    deliveryOrder.push(item);
+  }
+    return deliveryOrder;
+};
 
+function refundOrder(number,deliveryOrder) {
+  for (var i = 0; i < deliveryOrder.length; i++) {
+    if (deliveryOrder[i].orderNumber === number) {
+      deliveryOrder.splice([i],1)
+    }
+  }
+  return deliveryOrder;
+};
 
+function listItems(deliveryOrder) {
+  var newArray = [];
+    for (var i = 0; i < deliveryOrder.length; i++) {
+      newArray.push(deliveryOrder[i].item)
+    };
+  return newArray.join(', ');
+};
 
 
 
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
